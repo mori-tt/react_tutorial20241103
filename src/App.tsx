789dcdb78e5ecbb0ad6a48ad07c70efcc4a3c119
T_Mori <morit0301@gmail.com>
2024-11-03 17:20:34 +0900

    メモ作成フォームの実装 - 共通の型を定義 - メモ作成ロジックの追加 - Editorコンポーネントの作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,42 @@
-import Layout from "./components/layout";
+import Layout from "./components/Layout";
+import Editor from "./components/Editor";
+import { useState } from "react";
+import { Memo } from "./types";
+
+const initialMemo = {
+  id: Date.now(),
+  text: "思いついたことを、サッとメモ。\n作業に集中するための、シンプルなメモアプリです！",
+  timestamp: new Date().toLocaleString("ja-JP"),
+};
 
 function App(): JSX.Element {
-  return <Layout>hello</Layout>;
+  const [memos, setMemos] = useState<Memo[]>([initialMemo]);
+
+  const addMemo = (inputText: string) => {
+    if (inputText.trim()) {
+      const memo: Memo = {
+        id: Date.now(),
+        text: inputText,
+        timestamp: new Date().toLocaleString("ja-JP"),
+      };
+      setMemos([memo, ...memos]);
+    }
+  };
+
+  return (
+    <Layout>
+      <div className="w-3/4 flex-col space-y-4">
+        <Editor
+          placeholder="新しいメモを入力.."
+          type="shadow"
+          onSubmit={addMemo}
+        />
+      </div>
+      {memos.map((memo) => (
+        <p key={memo.id}>{memo.text}</p>
+      ))}
+    </Layout>
+  );
 }
 
 export default App;
